726c19baffc901bf2e9b826d7cd50b43aef3f8af
Tien Ta <taminhtien1993@gmail.com>
2015-10-02 22:36:50 +0700

    Add relationship between user and book

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151002151003) do
+ActiveRecord::Schema.define(version: 20151002153219) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -29,7 +29,10 @@ ActiveRecord::Schema.define(version: 20151002151003) do
     t.integer  "pages"
     t.string   "public_date"
     t.string   "categories"
+    t.integer  "user_id"
   end
+
+  add_index "books", ["user_id"], name: "index_books_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
