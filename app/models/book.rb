fe30d5213ace12c86f65b0cfa0bf524f5e7db4d4
Tien Ta <taminhtien1993@gmail.com>
2015-10-02 22:24:38 +0700

    Add User devise

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,6 @@ class Book < ActiveRecord::Base
 
 	has_attached_file :image, styles: { small: "135x189>", medium: "232x357>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+	belongs_to :user
 end
