409e2b1374c54b33941573f55cfdf72bf7f8f362
Tien Ta <taminhtien1993@gmail.com>
2015-10-07 20:32:04 +0700

    Add effect for current line item

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -34,7 +34,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to root_url }
-        format.js
+        format.js { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
