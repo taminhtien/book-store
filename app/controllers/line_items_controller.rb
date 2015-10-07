d1e39b6e40ebeefea94e2461370b9bba06cf508f
Tien Ta <taminhtien1993@gmail.com>
2015-10-07 18:34:24 +0700

    Add js response after creating line item

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -33,9 +33,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html {
-            redirect_to root_url
-          }
+        format.html { redirect_to root_url }
+        format.js
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
