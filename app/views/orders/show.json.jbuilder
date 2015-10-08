1454176982dad75be1acaf5c6ab9eceb3018e1d2
Tien Ta <taminhtien1993@gmail.com>
2015-10-08 23:29:37 +0700

    Add order model

---
+++ app/views/orders/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @order, :id, :name, :address, :email, :mobile, :pay_type, :created_at, :updated_at
---
+++ db/migrate/20151008144506_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.string :name
+      t.text :address
+      t.string :email
+      t.string :mobile
+      t.string :pay_type
+
+      t.timestamps null: false
+    end
+  end
+end
