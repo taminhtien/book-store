65f10476637aaf6b137aa3e5bf8e0efca3655735
Tien Ta <taminhtien1993@gmail.com>
2015-10-09 01:09:59 +0700

    Edit address form

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ActiveRecord::Base
+	PAYMENT_TYPES = [ "At destination place",
+		"Internet Banking",
+		"Visa, MasterCard" ]
+	validates :name, :address, :email, :mobile, presence: true
+	validates :pay_type, inclusion: PAYMENT_TYPES
 end
