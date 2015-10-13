9e7adbf19cd55b79fefd0a36e08c34d60c700295
Tien Ta <taminhtien1993@gmail.com>
2015-10-13 11:01:22 +0700

    Add new review modal bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree
+//= require bootstrap
 
 $(document).ready(function () {
   $('a.leave-review').click(function(event){
@@ -35,4 +36,6 @@ $(document).ready(function () {
     path: '/assets/',
     scoreName: 'review[rating]'
   });
+
+  
 });
